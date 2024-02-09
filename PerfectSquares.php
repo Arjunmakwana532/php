e3d9acf7f729fec8dd762d2a9b3d4f5257dc52e7
Arjun <arjunmakwana532@gmail.com>
2024-02-09 11:09:49 +0530

    update perfect squares

--- PerfectSquares.php
+++ PerfectSquares.php
@@ -1,5 +1,28 @@
 <?php
 
+// Given an integer n, return the least number of perfect square numbers that sum to n.
+
+// A perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself. For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
+
+ 
+
+// Example 1:
+
+// Input: n = 12
+// Output: 3
+// Explanation: 12 = 4 + 4 + 4.
+// Example 2:
+
+// Input: n = 13
+// Output: 2
+// Explanation: 13 = 4 + 9.
+ 
+
+// Constraints:
+
+// 1 <= n <= 104
+
+//----------------------------------------------------start----------------------------------------------------
 class Solution
 {
 
@@ -15,8 +38,9 @@ class Solution
 
         // Initialize an array to store the least number of perfect square numbers for each value from 1 to n
         $dp = array_fill(0, $n + 1, PHP_INT_MAX);
+
         // Base case: The least number of perfect square numbers to sum to 0 is 0
-        $dp[0] = 0;
+        $dp[0] = 0;     
 
         // Iterate through each number from 1 to n
         for ($i = 1; $i <= $n; $i++) {
